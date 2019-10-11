44dab0be0098a4afc8ab630a3892f01d8435e1af
Bjørn Vester <bmv@netcompany.com>
2019-10-11 14:27:32 +0200

    Upgrade OpenClover

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.openclover:clover:4.3.1")
+    implementation("org.openclover:clover:4.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 }
